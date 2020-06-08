97ea5dd425d0501b0af53234f766a22c41386643
Anthony Wang <anthony@anthonywang.com>
2020-06-08 09:41:17 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+TestModel.create!(datetime: '2020-01-01 12:34:56 UTC')
+TestModel.create!(datetime: '2020-01-01 12:34:56 PST')
