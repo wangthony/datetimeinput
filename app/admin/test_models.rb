2fcffd149d41a0c50f400bb60c48ac1684d7afe8
Anthony Wang <anthony@anthonywang.com>
2020-06-08 09:41:30 -0700

    add timezone display

--- app/admin/test_models.rb
+++ app/admin/test_models.rb
@@ -1,4 +1,20 @@
 ActiveAdmin.register TestModel do
+  actions :all, except: :show
+
+  index do
+    id_column
+    column :datetime do |obj|
+      obj.datetime&.strftime('%c %Z %z')
+    end
+    column :created_at do |obj|
+      obj.created_at&.strftime('%c %Z %z')
+    end
+    column :updated_at do |obj|
+      obj.updated_at&.strftime('%c %Z %z')
+    end
+    actions
+  end
+
   form do |f|
     f.inputs do
       f.input :datetime, as: :datepicker_with_time
