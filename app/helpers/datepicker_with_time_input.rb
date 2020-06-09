bc269602797693f5b4efa276d25b8c4bf4871673
prio101 <miprio101@gmail.com>
2020-06-09 22:23:09 +0600

    Added the Field For show only

--- app/helpers/datepicker_with_time_input.rb
+++ app/helpers/datepicker_with_time_input.rb
@@ -1,11 +1,9 @@
 class DatepickerWithTimeInput
   include ::Formtastic::Inputs::Base
   
-  
-
   def to_html
     input_wrapping do
-      label_html << builder.datetime_local_field(method, input_html_options) + time_zones 
+      label_html <<  builder.datetime_local_field(method, input_html_options) << time_zones 
     end
   end
 
