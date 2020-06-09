e643d47970f09e65534ca898ee323d41aafd4963
prio101 <miprio101@gmail.com>
2020-06-09 22:33:37 +0600

    added the Input Field

--- app/helpers/datepicker_with_time_input.rb
+++ app/helpers/datepicker_with_time_input.rb
@@ -3,15 +3,7 @@ class DatepickerWithTimeInput
   
   def to_html
     input_wrapping do
-      label_html <<  builder.datetime_local_field(method, input_html_options) << time_zones 
+      label_html <<  builder.datetime_local_field(method, input_html_options) 
     end
-  end
-
-  def time_zones
-    builder.select(method, zones_collection, input_options, input_html_options)
-  end
-
-  def zones_collection
-    ActiveSupport::TimeZone::MAPPING  
   end
 end
