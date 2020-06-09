1f532e47e0ba0c17f7e29bb7bb431b1082a6e576
prio101 <miprio101@gmail.com>
2020-06-09 20:29:32 +0600

    added the zone

--- app/helpers/datepicker_with_time_input.rb
+++ app/helpers/datepicker_with_time_input.rb
@@ -1,9 +1,19 @@
 class DatepickerWithTimeInput
   include ::Formtastic::Inputs::Base
+  
+  
 
   def to_html
     input_wrapping do
-      builder.datetime_local_field(method, input_html_options) 
+      label_html << builder.datetime_local_field(method, input_html_options) + time_zones 
     end
+  end
+
+  def time_zones
+    builder.select(method, zones_collection, input_options, input_html_options)
+  end
+
+  def zones_collection
+    ActiveSupport::TimeZone::MAPPING  
   end
 end
