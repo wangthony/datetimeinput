18832d85a03f0e2c3f57c5b68aafa717a683233c
prio101 <miprio101@gmail.com>
2020-06-12 22:00:48 +0600

    added the lastest changes

--- app/helpers/datepicker_with_time_input.rb
+++ app/helpers/datepicker_with_time_input.rb
@@ -1,9 +1,22 @@
 class DatepickerWithTimeInput
   include ::Formtastic::Inputs::Base
-  
+
   def to_html
     input_wrapping do
-      label_html <<  builder.datetime_local_field(method, input_html_options) 
+      label_html << date_field << time_html_input
     end
   end
+
+  def date_field
+    builder.date_field(method, input_html_options)
+  end
+
+  def input_html_options
+    super.merge(:class => "datetime")
+  end
+
+  def time_html_input
+    builder.template.time_field(method, input_html_options)
+  end
 end
+
